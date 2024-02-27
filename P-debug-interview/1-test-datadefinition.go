d33005eca0f2f9e51498bbc54334d2dccdaf075c
IAMLLT796 <lt.liu796@gmail.com>
2024-02-27 09:11:35 +0800

    函数返回值需要命名

--- P-debug-interview/1-test-datadefinition.go
+++ P-debug-interview/1-test-datadefinition.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 /*
 函数返回值需要有命名
 */
 func myFunc(x, y int) (sum int, err error) {
 	return x + y, nil
+}
+
+func main() {
+	res, err := myFunc(2, 5)
+	fmt.Println(res, err)
 }
