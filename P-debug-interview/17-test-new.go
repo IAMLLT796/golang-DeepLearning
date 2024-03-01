80796c36178abf3007065db6eaf4e3df11361103
IAMLLT796 <lt.liu796@gmail.com>
2024-03-01 09:49:47 +0800

    new和make的异同

--- P-debug-interview/17-test-new.go
+++ P-debug-interview/17-test-new.go
@@ -5,6 +5,12 @@ import (
 	"sync"
 )
 
+/*
+make 与 new 的异同
+1.都是用来堆空间分配内存的
+2.make 只用于 slice, map, channel 的初始化，无可替代
+3.new 用于类型内存分配， 初始值为0，不常用
+*/
 type user struct {
 	lock sync.Mutex
 	name string
