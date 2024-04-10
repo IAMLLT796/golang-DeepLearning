d9ace8cb84fed0b50754e4f3271c93febf6f9a46
IAMLLT796 <lt.liu796@gmail.com>
2024-04-10 10:07:03 +0800

    别名和新类型

---
+++ P-debug-basics/7-Varaible.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+type aliasInt = int // 定义一个类型别名
+type myInt int      // 定义一个新的类型
+
+func main() {
+	var alias aliasInt
+	fmt.Printf("alias value is %v, type is %T\n", alias, alias)
+
+	var myint myInt
+	fmt.Printf("myInt value is %v, type is %T\n", myint, myint)
+}
